e9a9eebc2472e12b11d916e2bfc25a3085dab83d
yukiomeira <yukiomeira@gmail.com>
2017-12-03 17:55:21 -0200

    Função de busca recursiva e grid de teste

--- pathfinder.py
+++ pathfinder.py
@@ -1,17 +1,18 @@
 grid = [[0, 0, 0, 0, 0, 1],
         [1, 1, 0, 0, 0, 1],
         [0, 0, 0, 1, 0, 0],
+        [0, 1, 1, 0, 0, 1],
         [0, 1, 0, 0, 1, 0],
         [0, 1, 0, 0, 0, 2]]
 
 # A função search(x,y) faz a verficação da célula visitada;
 def search(x,y):
-    if grid[x][y] == 2:
-        print("Encontrou célula final em: %d,%d" % (x, y))
-        return True
-    elif grid[x][y] == 1:
+    if grid[x][y] == 1:
         print("Parede em: %d,%d" % (x, y))
         return False
+    elif grid[x][y] == 2:
+        print("Encontrou célula final em: %d,%d" % (x, y))
+        return True
     elif grid[x][y] == 3:
         print("Visitado em: %d,%d" % (x, y))
         return False
@@ -22,10 +23,10 @@ def search(x,y):
     grid[x][y] = 3;
 
     # Explorando vizinhos em sentido horário começando pelo da direita
-    if ((x < len(grid) - 1 and search(x + 1, y))
-        or (y > 0 and search(x, y - 1))
-        or (x > 0 and search(x - 1, y))
-        or (y < len(grid) - 1 and search(x, y + 1))):
+    if ((x < len(grid)-1 and search(x+1, y))
+        or (y > 0 and search(x, y-1))
+        or (x > 0 and search(x-1, y))
+        or (y < len(grid)-1 and search(x, y+1))):
         return True
 
     return False
