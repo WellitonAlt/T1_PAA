e47d861f8d0a1429b48a5a0cc33b3e780d968688
Welliton Altimari <etoalt@hotmail.com>
2017-12-03 18:54:42 -0200

    Cores

--- gui.py
+++ gui.py
@@ -8,6 +8,10 @@ tela = pygame.display.set_mode([400,400])
 tela.fill((255, 255, 255))
 pygame.display.set_caption("PathFinding")
 
+
+img_verde = pygame.image.load('imagens/verde.jpg')
+img_vermelha = pygame.image.load('imagens/vermelho.jpg')
+
 matriz = ([[ 0,  1,  0,  1],
            [ 1,  0,  1,  0],
            [ 0,  1,  0,  1],
@@ -16,11 +20,10 @@ matriz = ([[ 0,  1,  0,  1],
 quad = pygame.Surface((40, 40))
 for i in range(0, 4):
     for j in range(0, 4):        
-        if matriz[i][j] == 0:
-            quad.fill((255, 0, 0))
+        if matriz[i][j] == 1:
+            tela.blit(img_verde, (i*40, j*40))
         else:
-            quad.fill((0, 255, 0))
-        tela.blit(quad, (i*40, j*40))
+            tela.blit(img_vermelha, (i*40, j*40))
 
         
 pygame.display.update()
