a157931e38f41bc4d7bcf1acaa1281b82d82e70b
yukiomeira <yukiomeira@gmail.com>
2017-12-03 19:10:11 -0200

    Preenchimento do Grid com as imagens

--- pathfinder.py
+++ pathfinder.py
@@ -1,9 +1,27 @@
+import pygame
+
+
+# Inicialização do pygame
+pygame.init()
+
+# Tamanho e nome da janela
+tela = pygame.display.set_mode([400,400])
+tela.fill((255, 255, 255))
+pygame.display.set_caption("PathFinding")
+
+img_verde = pygame.image.load('Imagens/Verde.jpg')
+img_vermelha = pygame.image.load('Imagens/Vermelho.jpg')
+img_preta = pygame.image.load('Imagens/Preto.jpg')
+img_azul = pygame.image.load('Imagens/Azul.jpg')
+img_branco = pygame.image.load('Imagens/Branco.jpg')
+
 grid = [[0, 0, 0, 0, 0, 1],
         [1, 1, 0, 0, 0, 1],
         [0, 0, 0, 1, 0, 0],
         [0, 1, 1, 0, 0, 1],
         [0, 1, 0, 0, 1, 0],
         [0, 1, 0, 0, 0, 2]]
+
 
 # A função search(x,y) faz a verficação da célula visitada;
 def search(x,y):
@@ -18,7 +36,8 @@ def search(x,y):
         return False
 
     print("Visitando %d,%d" % (x, y))
-
+    printGrid(x,y)
+    sleep(200)
     # Marcando como visitado
     grid[x][y] = 3;
 
@@ -31,4 +50,38 @@ def search(x,y):
 
     return False
 
+
+def printGridIni():
+    for i in range(0, 6):
+        for j in range(0, 6):
+            if grid[i][j] == 0:
+                tela.blit(img_branco, (i * 40, j * 40))
+            elif grid[i][j] == 1:
+                tela.blit(img_azul, (i * 40, j * 40))
+            elif grid[i][j] == 2:
+                tela.blit(img_vermelha, (i * 40, j * 40))
+            elif grid[i][j] == 3:
+                tela.blit(img_preta, (i * 40, j * 40))
+
+    tela.blit(img_preta, (0,0))
+    pygame.display.update()
+
+
+printGridIni()
+
+def printGrid(i ,j):
+    if grid[i][j] == 0:
+        tela.blit(img_branco, (i * 40, j * 40))
+    elif grid[i][j] == 1:
+         tela.blit(img_azul, (i * 40, j * 40))
+    elif grid[i][j] == 2:
+         tela.blit(img_vermelha, (i * 40, j * 40))
+    elif grid[i][j] == 3:
+         tela.blit(img_preta, (i * 40, j * 40))
+    pygame.display.update()
+
 search(0,0)
+
+
+
+input("Tecle algo para sair")
